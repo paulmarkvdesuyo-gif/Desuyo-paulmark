0ede4d50bb441012901e0bbdf7e98c3a8784b015
paulmarkvdesuyo-gif <paulmarkvdesuyo@gmail.com>
2026-02-06 19:01:13 +0800

    Delete salon-booking directory

--- salon-booking/auth/logout_admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-header('Location: login_admin_staff.php');
-exit;
-?>
--- salon-booking/sql/create_admin.sql
+++
@@ -1,4 +0,0 @@
-USE salon_booking;
-INSERT INTO users (name, email, password, role)
-VALUES ('admin', 'lovelycornea695@gmail.com', '{PASSWORD_HASH}', 'admin');
--- Replace {PASSWORD_HASH} with result of password_hash('yourpassword', PASSWORD_DEFAULT)
